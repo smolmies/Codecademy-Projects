12991691b6289c49ecbd6c25de199d9e3218e7cc
smolmies <70819166+smolmies@users.noreply.github.com>
2022-01-18 15:39:27 +0100

    Fix typo cipher file, add start of findhatproject

---
+++ Javascript Syntax III/find-hat-project/find-your-hat.js
@@ -0,0 +1,13 @@
+// For now this project is skipped as the level is too high for me right now.
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+class Field {
+    
+};
+
