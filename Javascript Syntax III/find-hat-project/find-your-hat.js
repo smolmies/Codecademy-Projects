18d56809f66b7858d0ef0540c037a71c06f8502b
smolmies <70819166+smolmies@users.noreply.github.com>
2022-01-19 11:33:57 +0100

    Added print, start of down move in findhatproject

--- Javascript Syntax III/find-hat-project/find-your-hat.js
+++ Javascript Syntax III/find-hat-project/find-your-hat.js
@@ -9,5 +9,41 @@ const pathCharacter = "*";
 
 class Field {
     
+    constructor(playingField){
+      this._playField = playingField;
+    }
+
+    print(){
+     this._playField.forEach(row => console.log(row.join("")));
+    }
+    moveInField(direction){
+      direction = direction.toLowerCase();
+      if(direction.toLowerCase() == "down"){
+        let foundRow = 0;
+        let foundIndex = 0;
+        this._playField.forEach((x, i) =>{
+          if(x.indexOf(pathCharacter) > -1){
+            foundRow = i;
+            foundIndex = x.indexOf(pathCharacter);
+          }
+        });
+        this._playField[foundRow][foundIndex] = fieldCharacter;
+        this._playField[foundRow+1][foundIndex] = pathCharacter;
+      }
+    }
 };
 
+const myField = new Field([
+  [pathCharacter, fieldCharacter, fieldCharacter],
+  [fieldCharacter, hole, hole],
+  [fieldCharacter, fieldCharacter, hat]
+]);
+
+myField.print();
+
+
+// while(){
+// let input = prompt("Giev move pls: ");
+// myField.moveInField(input);
+// myField.print();
+// }
